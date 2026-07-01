43d1b59a5ad52148abe7a15dd5ade999f4a2a87f
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-07-02 00:11:27 +0200

    Fix movement check

--- player.py
+++ player.py
@@ -20,37 +20,35 @@ class Player:
         draw_rectangle_v(self.position, Vector2(20, 20), BLUE)
 
     def move(self) -> None:
-        if not self.can_move:
-            return
+        if self.can_move:
+            delta: float = get_frame_time()
 
-        delta: float = get_frame_time()
+            if is_key_down(DefaultConfig.keybinds["walk_up"]):
+                self.movement.y = -1
+            if is_key_down(DefaultConfig.keybinds["walk_right"]):
+                self.movement.x = 1
+            if is_key_down(DefaultConfig.keybinds["walk_down"]):
+                self.movement.y = 1
+            if is_key_down(DefaultConfig.keybinds["walk_left"]):
+                self.movement.x = -1
 
-        if is_key_down(DefaultConfig.keybinds["walk_up"]):
-            self.movement.y = -1
-        if is_key_down(DefaultConfig.keybinds["walk_right"]):
-            self.movement.x = 1
-        if is_key_down(DefaultConfig.keybinds["walk_down"]):
-            self.movement.y = 1
-        if is_key_down(DefaultConfig.keybinds["walk_left"]):
-            self.movement.x = -1
+            if is_key_up(DefaultConfig.keybinds["walk_up"]) and is_key_up(DefaultConfig.keybinds["walk_down"]):
+                self.movement.y = 0
+            if is_key_up(DefaultConfig.keybinds["walk_left"]) and is_key_up(DefaultConfig.keybinds["walk_right"]):
+                self.movement.x = 0
 
-        if is_key_up(DefaultConfig.keybinds["walk_up"]) and is_key_up(DefaultConfig.keybinds["walk_down"]):
-            self.movement.y = 0
-        if is_key_up(DefaultConfig.keybinds["walk_left"]) and is_key_up(DefaultConfig.keybinds["walk_right"]):
-            self.movement.x = 0
+            length: float = vector2_length_sqr(self.movement)
 
-        length: float = vector2_length_sqr(self.movement)
+            if length > 0:
+                self.movement.x /= length
+                self.movement.y /= length
 
-        if length > 0:
-            self.movement.x /= length
-            self.movement.y /= length
+            if is_key_down(DefaultConfig.keybinds["sprint"]):
+                self.sprint = 1.25
+            else:
+                self.sprint = 1.0
 
-        if is_key_down(DefaultConfig.keybinds["sprint"]):
-            self.sprint = 1.25
-        else:
-            self.sprint = 1.0
-
-        self.position.x += self.movement.x * self.speed * self.sprint * delta
-        self.position.y += self.movement.y * self.speed * self.sprint * delta
+            self.position.x += self.movement.x * self.speed * self.sprint * delta
+            self.position.y += self.movement.y * self.speed * self.sprint * delta
 
         self.draw()
