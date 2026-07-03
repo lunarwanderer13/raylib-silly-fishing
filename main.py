545996d70ad5c8cccbb410b7b528322a2f345d3a
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-07-03 02:22:21 +0200

    Use dynamic config rather than default config

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from config import GameConfig
 from data import SaveManager
 
 def main() -> None:
+    save_manager: SaveManager = SaveManager("data.json")
+    data: dict[str, Any] = save_manager.load()
+    player: Player = Player(data)
+
     # The virtual resolution
     virtual_width: int = int(GameConfig.virtual_resolution.x)
     virtual_height: int = int(GameConfig.virtual_resolution.y)
@@ -13,14 +17,14 @@ def main() -> None:
     init_window(virtual_width * 3, virtual_height * 3, "Silly Fishing")
     set_window_state(ConfigFlags.FLAG_WINDOW_RESIZABLE)
 
-    set_target_fps(60)
+    if ((data["fullscreen"] and not is_window_state(ConfigFlags.FLAG_BORDERLESS_WINDOWED_MODE))
+            or (not data["fullscreen"] and is_window_state(ConfigFlags.FLAG_BORDERLESS_WINDOWED_MODE))):
+        toggle_borderless_windowed()
+
+    set_target_fps(data["fps"])
 
     target: RenderTexture = load_render_texture(virtual_width, virtual_height)
     set_texture_filter(target.texture, TextureFilter.TEXTURE_FILTER_POINT)
-
-    save_manager: SaveManager = SaveManager("data.json")
-    data: dict[str, Any] = save_manager.load()
-    player: Player = Player(data)
 
     while not window_should_close():
         # Calculate how much the resolution scale has to change to fit the 16:9 resolution
