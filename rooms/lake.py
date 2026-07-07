fdcff79c62e28fa85924d5e8b44401bc948ba9e3
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-07-07 18:50:32 +0200

    Add basic lake layout

--- rooms/lake.py
+++ rooms/lake.py
@@ -4,3 +4,8 @@ from config import GameConfig
 def draw_room() -> None:
     virtual_width: int = int(GameConfig.virtual_resolution.x)
     virtual_height: int = int(GameConfig.virtual_resolution.y)
+
+    draw_circle_v(Vector2(0, 0), 20, RED)
+    draw_rectangle_v(Vector2(virtual_width * 2/3, 0), Vector2(virtual_width * 1/3, virtual_height), DARKBLUE)
+    draw_rectangle_v(Vector2(virtual_width * 1/2, virtual_height / 2 - virtual_height / 10), Vector2(virtual_width * 1/3, virtual_height / 5), BROWN)
+    draw_rectangle_lines_ex(GameConfig.fishing_spots["lake"], 5, YELLOW)
