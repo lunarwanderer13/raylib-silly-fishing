61ec3bd5c70fbe28f2b0d8bc416fe9cb3c36803d
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-07-07 18:50:10 +0200

    Add a lake fishing spot

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ from pyray import *
 class GameConfig:
     virtual_resolution: Vector2 = Vector2(320, 180)
 
+    fishing_spots: dict[str, Rectangle] = {
+        "lake": Rectangle(virtual_resolution.x / 2 + 5, virtual_resolution.y / 2 - virtual_resolution.y / 10 + 5, virtual_resolution.x * 1/3 - 10, virtual_resolution.y / 5 - 10),
+    }
+
 class DefaultConfig:
     keybinds: dict[str, KeyboardKey] = {
         "walk_up": KeyboardKey.KEY_W,
