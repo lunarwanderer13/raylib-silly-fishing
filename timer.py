f83636a390a5281b10446e4aee9cfffa862a7e82
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-07-12 03:09:14 +0200

    Add timer resetting

--- timer.py
+++ timer.py
@@ -2,7 +2,11 @@ from pyray import get_frame_time
 
 class Timer:
     def __init__(self, time: float) -> None:
+        self.start_time = time
         self.time: float = time
 
     def update(self) -> None:
         self.time -= get_frame_time()
+
+    def reset(self) -> None:
+        self.time = self.start_time
